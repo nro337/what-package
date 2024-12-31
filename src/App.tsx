19900ea440218a72620f9ddb3dfaf1760f20c3c6
Nick Alico <nro337@gmail.com>
2024-12-30 20:15:49 -0500

    Check-in

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,16 @@ import {
   CardDescription,
   CardHeader,
   CardTitle,
-} from "./components/ui/card";
+} from "@/components/ui/card";
 import { PackageManagerSelect } from "./components/package-select";
-import { PackageDetails } from "./types/packages";
+import { CombinedPackageDetails, PackageDetails } from "./types/packages";
+import { Skeleton } from "@/components/ui/skeleton";
+import Sidebar from "./SidebarCard";
 
 function App() {
   const [search, setSearch] = useState<string>("");
   const [package_manager, setPackageManager] = useState<string>("");
+  const [cardSelected, setCardSelected] = useState<boolean>(false);
   // const [libraries_io_package_results, setLibrariesIOPackageResults] = useState<PackageDetails[]>([]);
   // const [combinedGHResults, setCombinedGHResults] = useState<CombinedPackageDetails[]>([]);
 
@@ -150,50 +153,82 @@ function App() {
             Search
           </Button> */}
         </div>
-        <CardContent className="grid gap-4">
-          {package_search_isLoading && (
-            <div className="flex flex-row place-content-center">
-              <LineWave
-                height={100}
-                width={100}
-                ariaLabel="Loading..."
-                firstLineColor="#f1f5f9"
-                middleLineColor="#0284c7"
-                lastLineColor="#374151"
-              />
-            </div>
-          )}
-          {package_search_data &&
-            package_search_data?.map((item: PackageDetails) => (
-              <Card key={item.name}>
-                <CardHeader>
-                  <CardTitle>{item.name}</CardTitle>
-                  <CardDescription>{item.description}</CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <p>Latest Version: {item.latest_stable_release_number}</p>
-                  <p>Stars: {item.stars}</p>
-                  <p>Rank: {item.rank}</p>
-                  <p>URL: {item.repository_url}</p>
-                  <p>
-                    {item.repository_url?.split("/").at(-2)}{" "}
-                    {item.repository_url?.split("/").at(-1)}
-                  </p>
-                  <p>
-                    {combinedPackageQueries.data.length > 0 &&
-                      !combinedPackageQueries.pending &&
-                      (
-                        combinedPackageQueries?.data.find(
-                          (e) =>
-                            (e.gh_package_data.data as PackageDetails)?.name ===
-                            item.repository_url?.split("/").at(-1)
-                        )?.gh_package_data.data as PackageDetails
-                      )?.forks}
-                  </p>
-                </CardContent>
-              </Card>
-            ))}
-        </CardContent>
+        <div className={cardSelected ? `grid grid-cols-2` : ``}>
+          <CardContent className="grid gap-4" onClick={() => setCardSelected(true)}>
+            {package_search_isLoading && (
+              <div className="flex flex-row place-content-center">
+                <LineWave
+                  height={100}
+                  width={100}
+                  ariaLabel="Loading..."
+                  firstLineColor="#f1f5f9"
+                  middleLineColor="#0284c7"
+                  lastLineColor="#374151"
+                />
+              </div>
+            )}
+            {package_search_data &&
+              package_search_data?.map((item: PackageDetails) => (
+                <Card key={item.name}>
+                  <div className="flex flex-row justify-center content-center pt-4">
+                    {combinedPackageQueries.pending ? (
+                      <Skeleton className="h-[100px] w-[100px] rounded-full" />
+                    ) : (
+                      <img
+                        src={
+                          (
+                            combinedPackageQueries?.data.find(
+                              (e) =>
+                                (
+                                  e.gh_package_data
+                                    .data as CombinedPackageDetails
+                                ).name ===
+                                item.repository_url?.split("/").at(-1)
+                            )?.gh_package_data.data as CombinedPackageDetails
+                          )?.owner.avatar_url
+                        }
+                        height={100}
+                        width={100}
+                        className="rounded-full"
+                      />
+                    )}
+                  </div>
+                  <CardHeader>
+                    <CardTitle>{item.name}</CardTitle>
+                    <CardDescription>{item.description}</CardDescription>
+                  </CardHeader>
+                  <CardContent>
+                    <p>Latest Version: {item.latest_stable_release_number}</p>
+                    <p>Stars: {item.stars}</p>
+                    <p>Rank: {item.rank}</p>
+                    <p>URL: {item.repository_url}</p>
+                    <p>
+                      {item.repository_url?.split("/").at(-2)}{" "}
+                      {item.repository_url?.split("/").at(-1)}
+                    </p>
+                    <p>
+                      {!combinedPackageQueries.pending &&
+                        (
+                          combinedPackageQueries?.data.find(
+                            (e) =>
+                              (e.gh_package_data.data as PackageDetails)
+                                ?.name ===
+                              item.repository_url?.split("/").at(-1)
+                          )?.gh_package_data.data as PackageDetails
+                        )?.forks}
+                    </p>
+                  </CardContent>
+                </Card>
+              ))}
+          </CardContent>
+          {
+            package_search_data && cardSelected && (
+              <CardContent className="grid gap-4">
+                <Sidebar />
+              </CardContent>
+            )
+          }
+        </div>
       </Card>
     </>
   );
